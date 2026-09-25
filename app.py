c3f0028d1bf034f826937370a7bd8765dfe13c3c
damminhbinh <damminhbinh@gmail.com>
2026-09-25 09:18:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -246,7 +246,7 @@ if user["role"] == "student":
                             user_content.append(uploaded_image)
                             
                         response = client.models.generate_content(
-                            model='gemini-2.5-pro',
+                            model='gemini-3.1-pro-preview',,
                             contents=user_content,
                             config=types.GenerateContentConfig(
                                 system_instruction=SYSTEM_INSTRUCTION,
